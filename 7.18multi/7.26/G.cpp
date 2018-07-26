3ae2b9d122a3209df4ab717afb1baa882a1a4eb8
Wavator <40030837+Wavator@users.noreply.github.com>
2018-07-26 22:25:35 +0800

    Update G.cpp

--- 7.18multi/7.26/G.cpp
+++ 7.18multi/7.26/G.cpp
@@ -1,60 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 5005;
-const int mod = 1000000007;
-inline int mult(int x, int y) {
-    return static_cast<int>((long long)x * y % mod);
+const int mod=1e9+7;
+int n,m,k,a=1,b=1,h,t,u,x,y;
+vector<int> e[5001];
+int c[5001],q[5001];
+void f(int u,int w,int d){
+    if(c[u]){if(d<k)x--;if(d<=k)y--;}
+    for(int&v:e[u])if(v^w)f(v,u,d+1);
 }
-struct edge {
-    int y;
-    edge * next;
-};
-edge * li[N];
-inline void insert(int u, int v) {
-    static edge e[N<<1], *top = e;
-    li[u] = & (*top++=(edge){v,li[u]});
-    li[v] = & (*top++=(edge){u,li[v]});
-}
-int n, m, k, dd;
-bool vis[N];
-void dfs(int u, int fa, int d, int &remains) {
-    if(vis[u]){if(d<dd)remains--;}
-    int v;
-    for(edge*t=li[u];t;t=t->next){
-        v=t->y;
-        if(v!=fa){
-            dfs(v,u,d+1,remains);
-        }
+int main(){
+    cin>>n>>m>>k;
+    for(u=1;u<n;++u){
+        cin>>x>>y;
+        e[x].push_back(y),e[y].push_back(x);
     }
-}
-
-int gao(int d) {
-    dd=d;
-    static int que[N];
-    int hd=0,tl=0,u;
-    fill(vis+1,vis+1+n,false);
-    int ans=1;
-    for(que[tl++]=1;hd^tl;hd++){
-        u=que[hd];
-        int t=m;
-        dfs(u,0,0,t);
-        ans=mult(ans,t);
-        vis[u]=true;
-        for(edge*x=li[u];x;x=x->next){
-            if(!vis[x->y])que[tl++]=x->y;
-        }
+    for(q[t++]=1;h^t;){
+        x=y=m;
+        f(u=q[h++],0,0);
+        a=1ll*x*a%mod;
+        b=1ll*y*b%mod;
+        c[u]=1;
+        for(int&v:e[u])if(!c[v])q[t++]=v;
     }
-    return ans;
-}
-
-int main() {
-#ifdef Wavator
-    freopen("test.in", "r", stdin);
-#endif
-    scanf("%d%d%d", &n, &m, &k);
-    for(int i = 1, u, v; i < n; ++i) {
-        scanf("%d%d", &u, &v);
-        insert(u, v);
-    }
-    return !printf("%d\n",(gao(k)-gao(k+1)+mod)%mod);
+    cout<<(a-b+mod)%mod;
 }
