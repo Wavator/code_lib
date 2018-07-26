e7d08243072d3f1e616369127e3b2a85d77005d0
Wavator <40030837+Wavator@users.noreply.github.com>
2018-07-26 22:30:48 +0800

    Update G.cpp

--- 7.18multi/7.26/G.cpp
+++ 7.18multi/7.26/G.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int mod=1e9+7;
-int n,m,k,a=1,b=1,h,t,u,x,y;
+int n,m,k,a=1,b=1,h,t,u,x,y,M=1e9+7;
 vector<int> e[5001];
 int c[5001],q[5001];
 void f(int u,int w,int d){
@@ -10,17 +9,17 @@ void f(int u,int w,int d){
 }
 int main(){
     cin>>n>>m>>k;
-    for(u=1;u<n;++u){
+    while(--n){
         cin>>x>>y;
         e[x].push_back(y),e[y].push_back(x);
     }
     for(q[t++]=1;h^t;){
         x=y=m;
         f(u=q[h++],0,0);
-        a=1ll*x*a%mod;
-        b=1ll*y*b%mod;
+        a=1ll*x*a%M;
+        b=1ll*y*b%M;
         c[u]=1;
         for(int&v:e[u])if(!c[v])q[t++]=v;
     }
-    cout<<(a-b+mod)%mod;
+    cout<<(a-b+M)%M;
 }
